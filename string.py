0c5a9455b329c19b90b6abae518eddf12bd814c0
bhushanhegde <45070488+bhushanhegde@users.noreply.github.com>
2019-04-25 18:14:31 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,112 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> bhushan hegde
+SyntaxError: invalid syntax
+>>> bhushanhegde
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    bhushanhegde
+NameError: name 'bhushanhegde' is not defined
+>>> "bhushan hegde"
+'bhushan hegde'
+>>> 'bhushan hegde'
+'bhushan hegde'
+>>> "bhushan hegde said"she is cute""
+SyntaxError: invalid syntax
+>>> 'bhushan hegde said"she is cute"'
+'bhushan hegde said"she is cute"'
+>>> 'i don't like her'
+SyntaxError: invalid syntax
+>>> "i don't like her"
+"i don't like her"
+>>> "she said "she don't know""
+SyntaxError: invalid syntax
+>>> "she said\"she don\,t know"
+'she said"she don\\,t know'
+>>> print(r"she said "she don't know"")
+      
+SyntaxError: invalid syntax
+>>> print('hey')
+hey
+>>> print(r'she said"she don't know"')
+      
+SyntaxError: invalid syntax
+>>> print(r'she said "she don\'t know"')
+she said "she don\'t know"
+>>> print("she said "she don't know"")
+      
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> bhushi="i like you"
+>>> hey+bhush
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    hey+bhush
+NameError: name 'hey' is not defined
+>>> "hey"+bhushi
+'heyi like you'
+>>> 'hey\t'+bhushi
+'hey\ti like you'
+>>> 'hey'\t+bhushi
+SyntaxError: unexpected character after line continuation character
+>>> "hey"+bhushi
+'heyi like you'
+>>> bhushi="\n i like you"
+>>> print(bhushi)
+
+ i like you
+>>> bhushi="\t i like you"
+>>> print(bhushi"
+      
+SyntaxError: EOL while scanning string literal
+>>> print(bhushi)
+	 i like you
+>>> "hey"+bhushi
+'hey\t i like you'
+>>> $hence \t and \n will be actvated only in the print function
+SyntaxError: invalid syntax
+>>> #hence \t and \n will be activated only in the print function
+>>> print("hey"+bhushi)
+hey	 i like you
+>>> print("c:user\python\file")
+c:user\pythonile
+>>> print(r"c:user\python\file")
+c:user\python\file
+>>> print("F:movies\number")
+F:movies
+umber
+>>> 
+>>> print(r"F:movies\number")
+SyntaxError: invalid syntax
+>>> print(r"F:movies\number")
+F:movies\number
+>>> a="bhushan"
+>>> a*10
+'bhushanbhushanbhushanbhushanbhushanbhushanbhushanbhushanbhushanbhushan'
+>>> a="bhushan\t"
+>>> a*10
+'bhushan\tbhushan\tbhushan\tbhushan\tbhushan\tbhushan\tbhushan\tbhushan\tbhushan\tbhushan\t'
+>>> print(a*10)
+bhushan	bhushan	bhushan	bhushan	bhushan	bhushan	bhushan	bhushan	bhushan	bhushan	
+>>> 
